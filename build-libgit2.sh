b693117928572106cad5f6bfef4ce85d1aa615f1
Kevin Sawicki <kevin@github.com>
2013-03-09 13:49:32 -0800

    Force built-in zlib library

--- build-libgit2.sh
+++ build-libgit2.sh
@@ -2,5 +2,5 @@ cd deps/libgit2
 touch .npmignore
 mkdir build
 cd build
-cmake .. -DCMAKE_C_FLAGS="$1" -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=1 -DBUILD_CLAR=0 -DBUILD_SHARED_LIBS=0
+cmake .. -DCMAKE_C_FLAGS="$1" -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=1 -DBUILD_CLAR=0 -DBUILD_SHARED_LIBS=0 -DZLIB_LIBRARY=1
 cmake --build .
