1046cf1e2a8a2d5cd01cbcf846647991891eb326
Kevin Sawicki <kevin@github.com>
2013-03-09 14:32:37 -0800

    Use built in SHA-1 type

--- build-libgit2.sh
+++ build-libgit2.sh
@@ -2,5 +2,7 @@ cd deps/libgit2
 touch .npmignore
 mkdir build
 cd build
-cmake .. -DCMAKE_C_FLAGS="$1" -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=1 -DBUILD_CLAR=0 -DBUILD_SHARED_LIBS=0 -DZLIB_LIBRARY=1
+cmake .. -DCMAKE_C_FLAGS="$1" -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=1 \
+         -DBUILD_CLAR=0 -DBUILD_SHARED_LIBS=0 -DZLIB_LIBRARY=1 \
+         -DSHA1_TYPE=builtin
 cmake --build .
