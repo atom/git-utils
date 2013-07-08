2d8319be3d59a5225df50225e175a0823ddfb0ff
Kevin Sawicki <kevin@github.com>
2013-07-07 20:52:43 -0700

    Use bundled zlib in libgit2 build

--- build-libgit2.sh
+++ build-libgit2.sh
@@ -5,5 +5,6 @@ touch .npmignore
 mkdir build
 cd build
 cmake .. -DCMAKE_C_FLAGS="$1" -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=1 \
-         -DBUILD_CLAR=0 -DBUILD_SHARED_LIBS=0 -DSHA1_TYPE=builtin
+         -DBUILD_CLAR=0 -DBUILD_SHARED_LIBS=0 -DZLIB_LIBRARY=0 \
+         -DSHA1_TYPE=builtin
 cmake --build .
